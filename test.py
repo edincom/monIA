2aafa6b31f5019a4dee40cfcfa3c3a9f72ac56a1
Edin Comor <195357@ecam.be>
2022-05-03 15:01:54 +0200

    Code précédent envoyé par erreur

--- test.py
+++ test.py
@@ -1,14 +1,24 @@
 import socket
 import json
-s = socket.socket()
-message = {
+
+m = {
    "request": "subscribe",
    "port": 8888,
    "name": "fun_name_for_the_client",
    "matricules": ["12345", "67890"]
 }
-jsonStr = json.dumps(message)
-s.connect(("localhost", 3000))
-s.sendall(bytes(message,encoding="utf-8"))
-print(s.recv(2048))
 
+data = json.dumps(m)
+s = socket.socket()
+
+s.connect(("localhost", 3000))
+s.sendall(bytes(data,encoding="utf-8"))
+
+
+received = s.recv(1024)
+received = received.decode("utf-8")
+
+
+
+
+print(received)
