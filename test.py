6b8f5b7be195404b5ec90ca67c82c410a3726121
Edin Comor <195357@ecam.be>
2022-05-03 14:59:40 +0200

    inscription au serveur réussie

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
 import socket
+import json
+s = socket.socket()
+message = {
+   "request": "subscribe",
+   "port": 8888,
+   "name": "fun_name_for_the_client",
+   "matricules": ["12345", "67890"]
+}
+jsonStr = json.dumps(message)
+s.connect(("localhost", 3000))
+s.sendall(bytes(message,encoding="utf-8"))
+print(s.recv(2048))
+
